c8afff86448afe6743489ff7c3a8b82f79ddddcf
Tao Guo <tao@temporal.io>
2024-08-26 19:58:18 +1000

    Support continue as new

--- fifo_signals/main.go
+++ fifo_signals/main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
+	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 	"go.temporal.io/sdk/workflow"
@@ -43,6 +44,7 @@ func run() error {
 	// Start worker
 	w := worker.New(c, taskQueue, worker.Options{})
 	w.RegisterWorkflow(FifoWorkflow)
+	w.RegisterActivity(Activity)
 	if err := w.Start(); err != nil {
 		return err
 	}
@@ -54,37 +56,34 @@ func run() error {
 	}
 	var run client.WorkflowRun
 
-	// Send first batch
-	for i := 0; i < 5; i++ {
-		run, err = c.SignalWithStartWorkflow(
-			ctx,
-			workflowID,
-			signalName,
-			strconv.Itoa(i),
-			wo,
-			FifoWorkflow,
-		)
-		if err != nil {
-			return err
+	concurrency := 10
+	maxMessages := 100
+	// Send concurrent signals in batches
+	for j := 0; j < 20; j++ {
+		var wg sync.WaitGroup
+		for i := 0; i < 20; i++ {
+			wg.Add(1)
+			myCounter := j*10 + i
+			go func() {
+				defer wg.Done()
+				run, err = c.SignalWithStartWorkflow(
+					ctx,
+					workflowID,
+					signalName,
+					fmt.Sprintf("belated message %d", myCounter),
+					wo,
+					FifoWorkflow,
+					concurrency,
+					maxMessages,
+					[]string{},
+				)
+				if err != nil {
+					panic(err)
+				}
+			}()
 		}
-	}
-
-	// Wait for
-	time.Sleep(2 * time.Second)
-
-	// Send second batch
-	for i := 0; i < 5; i++ {
-		run, err = c.SignalWithStartWorkflow(
-			ctx,
-			workflowID,
-			signalName,
-			fmt.Sprintf("belated message %d", i),
-			wo,
-			FifoWorkflow,
-		)
-		if err != nil {
-			return err
-		}
+		wg.Wait()
+		time.Sleep(1 * time.Second)
 	}
 
 	err = run.Get(context.Background(), nil)
@@ -97,25 +96,43 @@ func run() error {
 
 type Listener struct {
 	Messages []string
+	Max      int
+	Done     bool
 }
 
 func (l *Listener) Listen(ctx workflow.Context) {
+	chn := workflow.GetSignalChannel(ctx, signalName)
+	processed := 0
 	selector := workflow.NewSelector(ctx)
-	signalChn := workflow.GetSignalChannel(ctx, signalName)
-	selector.AddReceive(signalChn, func(c workflow.ReceiveChannel, more bool) {
+	selector.AddReceive(chn, func(c workflow.ReceiveChannel, more bool) {
 		var msg string
 		c.Receive(ctx, &msg)
+		processed++
 		l.Messages = append(l.Messages, msg)
 	})
 
 	for {
+		if processed > l.Max && chn.Len() == 0 {
+			l.Done = true
+			return
+		}
+
 		selector.Select(ctx)
 	}
 }
 
-func FifoWorkflow(ctx workflow.Context) (string, error) {
+func Activity(ctx context.Context, name string) (string, error) {
+	logger := activity.GetLogger(ctx)
+	logger.Info("Hello", "name", name)
+	return "Hello " + name + "!", nil
+}
+
+func FifoWorkflow(ctx workflow.Context, concurrency int, maxMessages int, messages []string) (string, error) {
 	logger := workflow.GetLogger(ctx)
-	l := Listener{Messages: []string{}}
+	l := Listener{
+		Messages: messages,
+		Max:      maxMessages,
+	}
 	workflow.Go(ctx, l.Listen)
 
 	for {
@@ -127,14 +144,31 @@ func FifoWorkflow(ctx workflow.Context) (string, error) {
 			return "", err
 		}
 
-		// Process messages
-		for len(l.Messages) > 0 {
+		// Execute activities concurrently
+		ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+			StartToCloseTimeout: time.Second * 5,
+		})
+		futures := []workflow.Future{}
+		for len(l.Messages) > 0 && len(futures) < concurrency {
 			var msg string
 			msg, l.Messages = l.Messages[0], l.Messages[1:]
-			logger.Info("Received", "msg", msg)
-			// execute your Activities
+			f := workflow.ExecuteActivity(ctx, Activity, msg)
+			futures = append(futures, f)
 		}
 
-		// TODO add continueAsNew
+		// Wait for the concurrent batch to complete
+		for _, f := range futures {
+			var result string
+			err := f.Get(ctx, &result)
+			if err != nil {
+				return "", err
+			}
+			logger.Info("Result", result)
+		}
+
+		// Continue as new if needed
+		if l.Done {
+			return "", workflow.NewContinueAsNewError(ctx, FifoWorkflow, concurrency, maxMessages, l.Messages)
+		}
 	}
 }
